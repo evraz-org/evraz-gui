3205ef68f3fb2423f6d91b573c95b35cd3c5b060
Dmitry <errtofordo@gmail.com>
2023-11-28 13:37:32 +0600

    Default unit EUR

--- app/stores/SettingsStore.js
+++ app/stores/SettingsStore.js
@@ -13,7 +13,7 @@ import {
     getUnits
 } from "branding";
 
-const CORE_ASSET = "BTS"; // Setting this to BTS to prevent loading issues when used with BTS chain which is the most usual case currently
+const CORE_ASSET = "EUR";
 
 const STORAGE_KEY = "__graphene__";
 let ss = ls(STORAGE_KEY);
@@ -466,17 +466,11 @@ class SettingsStore {
                 ]
             };
 
-            let coreAssets = {
-                markets_4018d784: "EUR",
-                markets_39f5e2ed: "TEST"
-            };
-            let coreAsset = coreAssets[this.starredKey] || "EUR";
             /*
              * Update units depending on the chain, also make sure the 0 index
              * asset is always the correct CORE asset name
              */
             this.onUpdateUnits();
-            this.defaults.unit[0] = coreAsset;
 
             let defaultBases = bases[this.starredKey] || bases.markets_4018d784;
             let storedBases = ss.get(this.basesKey, []);
